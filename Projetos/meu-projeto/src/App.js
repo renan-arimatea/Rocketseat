9b95b67034300c316494b1216b4905b302683870
Renan Arimatea <renanmoraiz@gmail.com>
2022-01-28 22:18:21 -0300

    Inicializando com React e as primeiras configurações.

---
+++ Projetos/meu-projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Olá React!</h1>
+      <p>Meu Primeiro APP em React</p>
+    </div>
+  );
+}
+
+export default App;
