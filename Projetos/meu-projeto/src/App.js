69f160dd289f7325c98b9b1ed494f3a1eb96e2cd
Renan Arimatea <renanmoraiz@gmail.com>
2022-02-03 17:38:48 -0300

    Trabalhando com conceitos de Css dentro de react.

--- Projetos/meu-projeto/src/App.js
+++ Projetos/meu-projeto/src/App.js
@@ -1,26 +1,24 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
 
-  const name = "Renan"
-
-  const newName = name.toUpperCase()
-
-  function soma(a, b) {
-    return (a + b)
-  }
-
-  const url = "https://via.placeholder.com/150"
+  const nome = "Dayana"
 
   return (
     <div className='App'>
-      <h1>Olá React!</h1>
-      <p>Meu Primeiro APP em React</p>
-      <p>Olá, {newName}</p>
-      <p>Soma: {soma(3,5)}</p>
-      <img src={url} alt='Minha Imagem'/>
-      <HelloWorld/>
+      <Frase />
+      <SayMyName nome="Renan"/>
+      <SayMyName nome="João"/>
+      <SayMyName nome={nome}/>
+      <Pessoa 
+      nome="Renan"
+      idade="26"
+      profissao="Desenvolvedor"
+      foto="https://via.placeholder.com/150"
+      />
     </div>
   );
 }
---
+++ Projetos/meu-projeto/src/components/SayMyName.js
@@ -0,0 +1,10 @@
+function SayMyName(props) {
+
+    return  (
+        <div>
+            <p>Fala aí {props.nome}, suave?</p>
+        </div>
+    )
+}
+
+export default SayMyName
