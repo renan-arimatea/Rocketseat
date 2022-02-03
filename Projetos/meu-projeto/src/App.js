50270cec0f88e233ad0da4433efdc2fa6ea928c7
Renan Arimatea <renanmoraiz@gmail.com>
2022-02-03 11:45:45 -0300

    Trabalhando conceitos de React.

--- Projetos/meu-projeto/src/App.js
+++ Projetos/meu-projeto/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 
 function App() {
+
+  const name = "Renan"
+
+  const newName = name.toUpperCase()
+
+  function soma(a, b) {
+    return (a + b)
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className='App'>
       <h1>Olá React!</h1>
       <p>Meu Primeiro APP em React</p>
+      <p>Olá, {newName}</p>
+      <p>Soma: {soma(3,5)}</p>
+      <img src={url} alt='Minha Imagem'/>
     </div>
   );
 }
