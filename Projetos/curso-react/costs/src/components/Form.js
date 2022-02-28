3e276bc947d401c9b7aac986e2f0e86e56b496c1
Renan Arimatea <renanmoraiz@gmail.com>
2022-02-28 08:39:28 -0300

    Trabalhando com Hooks e useState no React.

--- Projetos/curso-react/costs/src/components/Form.js
+++ Projetos/curso-react/costs/src/components/Form.js
@@ -1,16 +1,29 @@
+import {useState} from "react"
+
 function Form () {
 
     function cadastrarUsuario (event) {
         event.preventDefault()
-        console.log("Usuário cadastrado com successo!")
+        console.log(`Usuário: ${name} foi caddastrado com a senha: ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+
     return (
         <div>
             <h2>Meu cadastro:</h2>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome" />
+                    <label htmlFor="name"> Nome: </label>
+                    <input type="text" id="name" name="name" placeholder="Digite o seu nome" onChange={(event) => setName(event.target.value)}/>
                 </div>
+
+                <div>
+                    <label htmlFor="password"> Senha: </label>
+                    <input type="password" id="password" name="password" placeholder="Digite a sua senha" onChange={(event) => setPassword(event.target.value)}/>
+                </div>
+
                 <div>
                     <input type="submit" value="Cadastrar" />
                 </div>
