37358be54ee35ae99422505800375798ed803401
Renan Arimatea <renanmoraiz@gmail.com>
2022-02-28 12:51:41 -0300

    Trabalhando com React Icons.

--- Projetos/curso-react/costs/src/App.js
+++ Projetos/curso-react/costs/src/App.js
@@ -17,6 +17,7 @@ import Home from './pages/Home';
 import Contato from './pages/Contato';
 import Empresa from './pages/Empresa';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
 
@@ -94,6 +95,7 @@ function App() {
               <Route path='/empresa' element={<Empresa/>}></Route>
               <Route path='/contato' element={<Contato/>}></Route>
           </Routes>
+          <Footer/>
       </Router>
     </div>
   );
