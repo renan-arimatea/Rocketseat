e9f3b3a9b0954f6e83633c275e0f537077a47410
Renan Arimatea <renanmoraiz@gmail.com>
2022-03-01 13:33:10 -0300

    Primeiro projeto em React, inicio do aplicativo Costs para gerenciar os custos de preojetos diversos.

---
+++ Projetos/costs/src/components/projects/ProjectForm.jsx
@@ -0,0 +1,30 @@
+import Input from '../form/Input'
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+import styles from './ProjectForm.module.css'
+
+function ProjectForm({btnText}) {
+    return (
+      <form className={styles.form}>
+        <Input
+          type="text"
+          text="Nome do Projeto"
+          name="name"
+          placeholder="Insira o nome do projeto"
+        />
+
+        <Input
+          type="number"
+          text="Orçamento do Projeto"
+          name="budget"
+          placeholder="Insira o orçamento total"
+        />
+
+        <Select name="category_id" text="Selecione a Categoria"/>
+
+        <SubmitButton text={btnText}/>
+      </form>
+    );
+}
+
+export default ProjectForm
