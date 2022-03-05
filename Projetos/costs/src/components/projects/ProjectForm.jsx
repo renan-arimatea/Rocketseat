44522fdec88f49a845977a728d24b3fb5534a56e
Renan Arimatea <renanmoraiz@gmail.com>
2022-03-04 23:38:41 -0300

    Criado a API e o banco de dados do site.

--- Projetos/costs/src/components/projects/ProjectForm.jsx
+++ Projetos/costs/src/components/projects/ProjectForm.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
+
 import Input from '../form/Input'
 import Select from '../form/Select';
 import SubmitButton from '../form/SubmitButton';
 import styles from './ProjectForm.module.css'
 
 function ProjectForm({btnText}) {
+
+  const [categories, setCategories] = useState([])
+
+  useEffect(() =>{
+    
+    fetch("http://localhost:5000/categories", {
+    
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+  })
+
+    .then((resp) => resp.json())
+    .then((data) =>{
+      setCategories(data)
+    })
+    .catch((err) => console.log(err))
+  }, [])
+
     return (
       <form className={styles.form}>
         <Input
@@ -20,7 +42,7 @@ function ProjectForm({btnText}) {
           placeholder="Insira o orçamento total"
         />
 
-        <Select name="category_id" text="Selecione a Categoria"/>
+        <Select name="category_id" text="Selecione a Categoria" options={categories}/>
 
         <SubmitButton text={btnText}/>
       </form>
